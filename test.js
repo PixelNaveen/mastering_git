a05178c4af7b41c24be09c6236c8602888c46c74
Naveen <naveen.knj19@gmail.com>
2025-07-01 13:02:55 +0530

    restore the codes

--- test.js
+++ test.js
@@ -2,13 +2,13 @@ console.log('Hello GitHub');
 console.log('Hello Git');
 console.log('Hello');
 
-<<<<<<< Updated upstream
+
 //I am a comment
 // I am another comment
 // I am a third comment
-=======
+
 // I am a test file
 // This is a test file for GitHub
 // This is a test file for Git
 // This is a test file for GitHub and Git
->>>>>>> Stashed changes
+
